0e12552794295899453aa8b2ba5bbcf3977d84f8
youngthe <whdudwns7100@naver.com>
2021-12-01 14:53:57 +0900

    add polls project

--- shrinkers/settings.py
+++ shrinkers/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shortener.apps.ShortenerConfig'
+    'shortener.apps.ShortenerConfig',
+    'polls.apps.PollsConfig'
 ]
 
 MIDDLEWARE = [
