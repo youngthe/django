8839a7f9993e6e5f8cae537ed67589ca9f200ac3
Cho Young Jun <whdudwns7100@naver.com>
2021-12-04 18:09:51 +0900

    transformation to Generic View

--- polls/views.py
+++ polls/views.py
@@ -7,25 +7,20 @@ from django.urls import reverse
 from .models import Question, Choice
 from django.views import generic
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name='latest_question_list'
 
-def detail(request, question_id):
-    # try:
-        # question = Question.objects.get(pk=question_id)
-    question = get_object_or_404(Question, pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
